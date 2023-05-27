2dc151b5ecb51bdfc768bffb67c2ab049e7f1a34
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-05-27 12:41:34 +0200

    add install for ncurses compat

--- aws/main.tf
+++ aws/main.tf
@@ -42,6 +42,7 @@ resource "aws_spot_instance_request" "cuda" {
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
+              sudo yum install -y ncurses-compat-libs
               printf 'export PATH="/usr/local/cuda/bin:$PATH"\nexport LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"\n' >> /home/ec2-user/.bashrc
               EOF
 }
