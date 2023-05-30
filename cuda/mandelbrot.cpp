eb1757d8fe2b1f0bfb2e062ff24b95ebcbcdb5d6
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-05-30 19:16:19 +0200

    cleanup

--- cuda/mandelbrot.cpp
+++ cuda/mandelbrot.cpp
@@ -11,15 +11,12 @@
 #include <complex>
 #include <fstream>
 
-#define MIN_X -2
-#define MAX_X 2
-#define MIN_Y -2
-#define MAX_Y 2
-#define MAX_COLOR 65535
-
+#define MIN -2
+#define MAX 2
+#define MAX_COLOR 3 * 255
 using namespace std;
 
-void generate_ppm(const int *pixels, int width, int height, string filename)
+void generate_ppm(const int *pixels, int size, string filename)
 {
     ofstream ppm_file(filename);
     if (!ppm_file)
@@ -29,11 +26,23 @@ void generate_ppm(const int *pixels, int width, int height, string filename)
     }
 
     ppm_file << "P3" << endl;
-    ppm_file << width << " " << height << " " << MAX_COLOR << endl;
+    ppm_file << size << " " << size << " " << MAX_COLOR / 3 << endl;
 
-    for (int i = 0; i < width * height; ++i)
+    for (int i = 0; i < size * size; ++i)
     {
-        ppm_file << "0 0 " << pixels[i] << endl;
+        int pixel = pixels[i];
+        if (pixel <= 255)
+        {
+            ppm_file << pixel << " 0 0 " << endl;
+        }
+        else if (pixel <= 510)
+        {
+            ppm_file << "0 " << pixel - 255 << " 0 " << endl;
+        }
+        else
+        {
+            ppm_file << "0 0 " << pixel - 510 << " " << endl;
+        }
     }
 
     ppm_file.close();
@@ -51,19 +60,18 @@ int calculate_mandelbrot_s(complex<float> c, int max_iteration)
     return iteration;
 }
 
-void calculate_mandelbrot_set_s(int width, int height, int max_iteration, int *mandelbrot_set)
+void calculate_mandelbrot_set_s(int size, int max_iteration, int *mandelbrot_set)
 {
-    float dx = (float)(MAX_X - MIN_X) / width;
-    float dy = (float)(MAX_Y - MIN_Y) / height;
-    for (int i = 0; i < width; i++)
+    float d = (float)(MAX - MIN) / size;
+    for (int i = 0; i < size; i++)
     {
-        float x = (float)MIN_X + i * dx;
-        for (int j = 0; j < height; j++)
+        float x = (float)MIN + i * d;
+        for (int j = 0; j < size; j++)
         {
-            float y = (float)MIN_Y + j * dy;
+            float y = (float)MIN + j * d;
             complex<float> c(x, y);
             int iterations = calculate_mandelbrot_s(c, max_iteration);
-            mandelbrot_set[j * height + i] = MAX_COLOR - ((MAX_COLOR * iterations) / max_iteration);
+            mandelbrot_set[j * size + i] = MAX_COLOR - ((MAX_COLOR * iterations) / max_iteration);
         }
     }
 }
@@ -81,18 +89,18 @@ __device__ int calculate_mandelbrot_r(cuComplex c, int max_iteration)
     return iteration;
 }
 
-__global__ void calculate_mandelbrot_set_r(int width, int height, int max_iteration, int *mandelbrot_set)
+__global__ void calculate_mandelbrot_set_r(int size, int max_iteration, int *mandelbrot_set)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (i < width && j < height)
+    if (i < size && j < size)
     {
-        float x = (float)MIN_X + (float)i * (MAX_X - MIN_X) / width;
-        float y = (float)MIN_Y + (float)j * (MAX_Y - MIN_Y) / height;
+        float x = (float)MIN + (float)i * (MAX - MIN) / size;
+        float y = (float)MIN + (float)j * (MAX - MIN) / size;
         cuComplex c = make_cuComplex(x, y);
         int iterations = calculate_mandelbrot_r(c, max_iteration);
-        mandelbrot_set[j * height + i] = MAX_COLOR - ((MAX_COLOR * iterations) / max_iteration);
+        mandelbrot_set[j * size + i] = MAX_COLOR - ((MAX_COLOR * iterations) / max_iteration);
     }
 }
 #endif
@@ -100,19 +108,17 @@ __global__ void calculate_mandelbrot_set_r(int width, int height, int max_iterat
 int main(int argc, char const *argv[])
 {
     string type = "c";
-    int width = 400;
-    int height = 400;
+    int size = 400;
     int max_iteration = 100;
     string filename = "";
     if (argc == 1)
     {
-        cout << "Usage: " << argv[0] << " [type] [width] [height] [max_iteration] [filename]" << endl
+        cout << "Usage: " << argv[0] << " [type] [size] [max_iteration] [filename]" << endl
              << endl;
         cout << "type:" << endl;
         cout << "c - compare results, s - serial, p - parallel" << endl
              << endl;
-        cout << "width: width of image" << endl;
-        cout << "height: height of image" << endl
+        cout << "size: size of image" << endl
              << endl;
         cout << "max_iteration: number of iterations" << endl
              << endl;
@@ -131,45 +137,44 @@ int main(int argc, char const *argv[])
     }
     if (argc > 2)
     {
-        width = atoi(argv[2]);
+        size = atoi(argv[2]);
     }
     if (argc > 3)
     {
-        height = atoi(argv[3]);
+        max_iteration = atoi(argv[3]);
     }
     if (argc > 4)
     {
-        max_iteration = atoi(argv[4]);
-    }
-    if (argc > 5)
-    {
-        filename = argv[5];
+        filename = argv[4];
     }
 
     chrono::time_point<chrono::system_clock> start, end;
     if (type == "c")
     {
-        int *mandelbrot_set_s = (int *)malloc(sizeof(int) * width * height);
+        int *mandelbrot_set_s = (int *)malloc(sizeof(int) * size * size);
         start = std::chrono::system_clock::now();
-        calculate_mandelbrot_set_s(width, height, max_iteration, mandelbrot_set_s);
+        calculate_mandelbrot_set_s(size, max_iteration, mandelbrot_set_s);
         end = std::chrono::system_clock::now();
         chrono::duration<double> time_s = end - start;
 
-        int *mandelbrot_set_p = (int *)malloc(sizeof(int) * width * height);
+        int *mandelbrot_set_p = (int *)malloc(sizeof(int) * size * size);
+        chrono::time_point<chrono::system_clock> cuda_dev_start, cuda_dev_end;
         start = std::chrono::system_clock::now();
 #ifdef __NVCC__
         int *cuda_mandelbrot_set;
-        cudaMalloc((void **)&cuda_mandelbrot_set, sizeof(int) * width * height);
+        cudaMalloc((void **)&cuda_mandelbrot_set, sizeof(int) * size * size);
         dim3 blockSize(32, 32);
-        dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
+        dim3 gridSize((size + blockSize.x - 1) / blockSize.x, (size + blockSize.y - 1) / blockSize.y);
 #endif
 
+        cuda_dev_start = std::chrono::system_clock::now();
 #ifdef __NVCC__
-        calculate_mandelbrot_set_r<<<gridSize, blockSize>>>(width, height, max_iteration, cuda_mandelbrot_set);
+        calculate_mandelbrot_set_r<<<gridSize, blockSize>>>(size, max_iteration, cuda_mandelbrot_set);
         cudaDeviceSynchronize();
 #else
-        calculate_mandelbrot_set_s(width, height, max_iteration, mandelbrot_set_p);
+        calculate_mandelbrot_set_s(size, size, max_iteration, mandelbrot_set_p);
 #endif
+        cuda_dev_end = std::chrono::system_clock::now();
 
 #ifdef __NVCC__
         cudaError_t err = cudaGetLastError();
@@ -180,53 +185,55 @@ int main(int argc, char const *argv[])
         }
 
         int *host_mandelbrot_set;
-        cudaMallocHost((void **)&host_mandelbrot_set, sizeof(int) * width * height);
-        cudaMemcpy(host_mandelbrot_set, cuda_mandelbrot_set, sizeof(int) * width * height, cudaMemcpyDeviceToHost);
-        memcpy(mandelbrot_set_p, host_mandelbrot_set, sizeof(int) * width * height);
+        cudaMallocHost((void **)&host_mandelbrot_set, sizeof(int) * size * size);
+        cudaMemcpy(host_mandelbrot_set, cuda_mandelbrot_set, sizeof(int) * size * size, cudaMemcpyDeviceToHost);
+        memcpy(mandelbrot_set_p, host_mandelbrot_set, sizeof(int) * size * size);
         cudaFreeHost(host_mandelbrot_set);
         cudaFree(cuda_mandelbrot_set);
 #endif
         end = std::chrono::system_clock::now();
         chrono::duration<double> time_p = end - start;
-        printf("Time for sequential: %f\n", time_s.count());
-        printf("Time for parallel: %f\n", time_p.count());
+        chrono::duration<double> time_dev = cuda_dev_end - cuda_dev_start;
+        // printf("Time for sequential: %f\n", time_s.count());
+        // printf("Time for parallel: %f\n", time_p.count());
+        printf("%d,%f,%f,%f\n", size, time_s.count(), time_p.count(), time_dev.count());
         if (filename != "")
         {
-            generate_ppm(mandelbrot_set_p, width, height, filename);
+            generate_ppm(mandelbrot_set_p, size, filename);
         }
         free(mandelbrot_set_s);
         free(mandelbrot_set_p);
     }
     else if (type[0] == 's' || type[1] == 's')
     {
-        int *mandelbrot_set = (int *)malloc(sizeof(int) * width * height);
+        int *mandelbrot_set = (int *)malloc(sizeof(int) * size * size);
         start = std::chrono::system_clock::now();
-        calculate_mandelbrot_set_s(width, height, max_iteration, mandelbrot_set);
+        calculate_mandelbrot_set_s(size, max_iteration, mandelbrot_set);
         end = std::chrono::system_clock::now();
         chrono::duration<double> time = end - start;
         printf("Time for sequential: %f\n", time.count());
         if (filename != "")
         {
-            generate_ppm(mandelbrot_set, width, height, filename);
+            generate_ppm(mandelbrot_set, size, filename);
         }
         free(mandelbrot_set);
     }
     else
     {
-        int *mandelbrot_set = (int *)malloc(sizeof(int) * width * height);
+        int *mandelbrot_set = (int *)malloc(sizeof(int) * size * size);
         start = std::chrono::system_clock::now();
 #ifdef __NVCC__
         int *cuda_mandelbrot_set;
-        cudaMalloc((void **)&cuda_mandelbrot_set, sizeof(int) * width * height);
+        cudaMalloc((void **)&cuda_mandelbrot_set, sizeof(int) * size * size);
         dim3 blockSize(32, 32);
-        dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
+        dim3 gridSize((size + blockSize.x - 1) / blockSize.x, (size + blockSize.y - 1) / blockSize.y);
 #endif
 
 #ifdef __NVCC__
-        calculate_mandelbrot_set_r<<<gridSize, blockSize>>>(width, height, max_iteration, cuda_mandelbrot_set);
+        calculate_mandelbrot_set_r<<<gridSize, blockSize>>>(size, max_iteration, cuda_mandelbrot_set);
         cudaDeviceSynchronize();
 #else
-        calculate_mandelbrot_set_s(width, height, max_iteration, mandelbrot_set);
+        calculate_mandelbrot_set_s(size, size, max_iteration, mandelbrot_set);
 #endif
 
 #ifdef __NVCC__
@@ -238,9 +245,9 @@ int main(int argc, char const *argv[])
         }
 
         int *host_mandelbrot_set;
-        cudaMallocHost((void **)&host_mandelbrot_set, sizeof(int) * width * height);
-        cudaMemcpy(host_mandelbrot_set, cuda_mandelbrot_set, sizeof(int) * width * height, cudaMemcpyDeviceToHost);
-        memcpy(mandelbrot_set, host_mandelbrot_set, sizeof(int) * width * height);
+        cudaMallocHost((void **)&host_mandelbrot_set, sizeof(int) * size * size);
+        cudaMemcpy(host_mandelbrot_set, cuda_mandelbrot_set, sizeof(int) * size * size, cudaMemcpyDeviceToHost);
+        memcpy(mandelbrot_set, host_mandelbrot_set, sizeof(int) * size * size);
         cudaFreeHost(host_mandelbrot_set);
         cudaFree(cuda_mandelbrot_set);
 #endif
@@ -249,7 +256,7 @@ int main(int argc, char const *argv[])
         printf("Time for parallel: %f\n", time.count());
         if (filename != "")
         {
-            generate_ppm(mandelbrot_set, width, height, filename);
+            generate_ppm(mandelbrot_set, size, filename);
         }
         free(mandelbrot_set);
     }
--- cuda/test.sh
+++ cuda/test.sh
@@ -1,52 +1,16 @@
 #!/bin/bash
-n_start=4
+n_start=20
 n_current=$n_start
-n_end=14
-step=1
+n_end=1000
+step=20
 
 if [ -f result.txt ]; then
     rm result.txt
 fi
 
 while [ $n_current -le $n_end ]; do
-    for i in {1..10}; do
-        ./hamilton s $n_current 0.6 1 >>result.txt
-    done
-    let n_current=n_current+step
-done
-
-level=1
-let n_current=n_start+level-1
-while [ $n_current -le $n_end ]; do
-    for i in {1..10}; do
-        ./hamilton p $n_current 0.6 $level >>result.txt
-    done
-    let n_current=n_current+step
-done
-
-level=2
-let n_current=n_start+level-1
-while [ $n_current -le $n_end ]; do
-    for i in {1..10}; do
-        ./hamilton p $n_current 0.6 $level >>result.txt
-    done
-    let n_current=n_current+step
-done
-
-level=3
-let n_current=n_start+level-1
-while [ $n_current -le $n_end ]; do
-    for i in {1..10}; do
-        ./hamilton p $n_current 0.6 $level >>result.txt
-    done
-    let n_current=n_current+step
-done
-
-level=7
-let n_current=n_start+level-1
-while [ $n_current -le $n_end ]; do
-    for i in {1..10}; do
-        ./hamilton p $n_current 0.6 $level >>result.txt
+    for i in {1..5}; do
+        ./mandelbrot c $n_current 1000 >>result.txt
     done
     let n_current=n_current+step
 done
